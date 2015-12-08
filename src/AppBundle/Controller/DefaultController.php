2e2b80f8240fcd6f08dab7a6dd2d871f9faf6769
stachura93 <b.stachura93@gmail.com>
2015-12-08 03:00:20 +0100

    add Category choose one main page

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -3,8 +3,10 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Product;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Request;
 
 use FOS\UserBundle\Model\UserManagerInterface;
@@ -14,14 +16,38 @@ class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
+     *
      */
     public function indexAction(Request $request)
     {
+      $em = $this->getDoctrine()->getManager();
+      $repository = $em->getRepository('AppBundle:Category');
 
+      $query = $repository->createQueryBuilder('c')
+      ->where('c.parent IS NULL')
+      ->getQuery();
 
+        $entities = $query->getResult();
       // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
-                'products' => "ssd",
+        return $this->render('default/index.html.twig', array('entities' => $entities));
+    }
+
+    /**
+     * Finds and displays a Category entity.
+     *
+     * @Route("/{name}", name="parent_category")
+     * @Method("GET")
+     * @Template()
+     */
+    public function showAction($name) {
+      $em = $this->getDoctrine()->getManager();
+
+      $id = $em->getRepository('AppBundle:Category')->findBy(array('name' => $name ));
+
+      $cat = $em->getRepository('AppBundle:Category')->findBy(array('parent' => $id ));
+
+
+      return $this->render('default/index.html.twig', array('entities' => $cat,
         ));
     }
 
